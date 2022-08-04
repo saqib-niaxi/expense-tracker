321000dcdfe8b31851add34617adf6ab579d4088
Muhammad Saqib Khan <91668456+saqib-niaxi@users.noreply.github.com>
2022-08-04 22:51:25 +0500

    Updates

---
+++ precache-manifest.ed97ae509f2c772dcdde1c8d13a7f79a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "263d592213f95c67518f959e98411d6a",
+    "url": "/expense-tracker/index.html"
+  },
+  {
+    "revision": "1e437c8822a8b5010191",
+    "url": "/expense-tracker/static/css/main.4ed2d42b.chunk.css"
+  },
+  {
+    "revision": "7b8995691a0dcfecdb39",
+    "url": "/expense-tracker/static/js/2.e2317e69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/expense-tracker/static/js/2.e2317e69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e437c8822a8b5010191",
+    "url": "/expense-tracker/static/js/main.a68b1ed5.chunk.js"
+  },
+  {
+    "revision": "fa66876fd3452fb7f6b0",
+    "url": "/expense-tracker/static/js/runtime-main.8f4c7c50.js"
+  }
+]);
